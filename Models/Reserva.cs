d463900e095ad467a51c14b68fe1de2966187efe
Jefferson Eduardo <46302791+jeffhawk@users.noreply.github.com>
2024-04-21 23:03:08 -0300

    finalizando desafio

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +25,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception ("Quantidade de hóspedes excede a capacidade do quarto");
             }
         }
 
@@ -37,7 +38,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            int qtdeHospedes = Hospedes.Count;
+            return qtdeHospedes;
         }
 
         public decimal CalcularValorDiaria()
@@ -49,9 +51,13 @@ namespace DesafioProjetoHospedagem.Models
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor = (DiasReservados * Suite.ValorDiaria) * 0.9m;
+            }
+            else
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
             }
 
             return valor;
